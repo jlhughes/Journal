eb7eaa6de33f57868af5a299c27dca7d32b2f541
Joel Hughes <joel@nanopay.net>
2026-01-04 15:46:24 -0500

    foamchat - include chat support from foam foamchat application

--- pom.js
+++ pom.js
@@ -1,10 +1,13 @@
 foam.POM({
   name: 'journal',
   vendorId: 'hughes',
-  version: '1.2.0',
+  version: '1.3.0',
   java: 21,
   projects: [
     { name: 'foam3/pom' },
-    { name: 'src/hughes/pom' }
+    { name: 'src/hughes/pom' },
+    { name: 'foamchat/src/com/foamdev/chat/pom'},
+    { name: "foamchat/webroot/pom" },
+    { name: "foamchat/journals/pom" }
   ]
 });
